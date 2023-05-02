0fe8acb185aac268ff199a7ff0da7c00ae983e2c
bpx-network <admin@bpxcoin.cc>
2023-05-02 15:02:38 +0200

    Add bpx config

--- config.sample.inc.php
+++ config.sample.inc.php
@@ -5,4 +5,9 @@ define('DB_USER', 'bx_feeder');
 define('DB_PASS', 'password');
 define('DB_NAME', 'beacon_bx');
 
+define('BPX_HOST', '127.0.0.1');
+define('BPX_PORT', 8201);
+define('BPX_CRT', '~/.bpx/beacon/config/ssl/beacon/private_beacon.crt');
+define('BPX_KEY', '~/.bpx/beacon/config/ssl/beacon/private_beacon.key');
+
 ?>
